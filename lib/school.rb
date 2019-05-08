c562a536796857e97a6aeb0548e237d8d3d1070a
Caleb Aviles <earthctzn1@gmail.com>
2019-05-08 02:17:16 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+
+class School
+  
+  
+  def initialize(name)
+    @name = name
+    @roster = {} 
+  end
+  
+  def roster 
+    @roster
+  end
+    
+  
+  def add_student(student, att)
+   # binding.pry
+    roster[att] = [] if !roster.keys.include?(att)
+    roster[att] << student
+  end
+  
+  def grade(n)
+    roster[n]
+  end 
+  def sort 
+    sorted = {}
+    roster.each {|k, v| sorted[k] = v.sort} 
+    sorted
+  end
+    
+  
+end 
