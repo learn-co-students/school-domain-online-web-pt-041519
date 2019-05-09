788f50618c280c464ce6ef9e79587be00ec37a16
Michael Chen <michaelchengaming@gmail.com>
2019-05-09 13:01:46 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
+require "pry"
 # code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    roster[grade] ||= [] #if !roster.include?(grade)
+    roster[grade] << student
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    sorted = {}
+    roster.each {|grade, student| sorted[grade] = student.sort}
+    sorted
+  end
+
+end
