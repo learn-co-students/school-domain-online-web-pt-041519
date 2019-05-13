ffe3091c9746d3d21908d9d08272f77b9e0ba98a
Jenna Ransom <jenna.ransom@gmail.com>
2019-05-13 06:31:14 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = { }
+    # or
+    # @roster = Hash.new {|h,k| h[k] = [ ]} and would only need "roster[grade] << name in method below"
+  end
+
+  def add_student(name, grade)
+    roster[grade] ||= [ ]
+    roster[grade] << name
+    # or
+    #if roster[grade]
+      #roster[grade] << name
+    #else
+      #roster[grade] = [name]
+    #end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort 
+     roster.each do |k,v|
+       v.sort!
+     end
+  end
+  
+end
