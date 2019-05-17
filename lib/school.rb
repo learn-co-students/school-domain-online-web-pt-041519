1efa3835a27999b9d8b69d20faa68b213c20ffb8
Chris Kay <chriskay25@gmail.com>
2019-05-17 14:24:36 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+
+class School
+  
+  attr_reader :roster
+  
+  def initialize(school)
+    @school = school 
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @name = name 
+    @grade = grade
+    if @roster.key?(grade)
+      @roster[@grade] << @name 
+    else 
+      @roster[@grade] = []
+      @roster[@grade] << @name
+    end
+  end 
+  
+  def grade(grade)
+    @grade = grade
+    @roster[@grade]
+  end 
+  
+  def sort
+    @roster.each do |grade, name|
+		name.sort!
+	end
+  end 
+  
+  
+  
+end
